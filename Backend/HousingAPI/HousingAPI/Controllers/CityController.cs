f84a6a986e13826673cc084ccf02e15c4fe9af99
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2022-08-09 09:41:12 +0530

    Create Backend project in asp.net core

---
+++ Backend/HousingAPI/HousingAPI/Controllers/CityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+    }
+}
