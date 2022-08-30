04e1e0568acfae15b961c6c4028afeee87712054
Sandeep Kumar <sandeepkumar.sk5@outlook.com>
2022-08-30 10:42:36 +0530

    use retry and whenRetry if API is down

--- Frontend/src/app/services/httperror-intercepter.service.ts
+++ Frontend/src/app/services/httperror-intercepter.service.ts
@@ -1,7 +1,8 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Observable, of, throwError } from "rxjs";
+import { catchError, concatMap, retry, retryWhen } from "rxjs/operators";
+import { ErrorCode } from "../enums/enums";
 import { AlertyfyService } from "./alertyfy.service";
 
 @Injectable({
@@ -14,6 +15,7 @@ export class HttpErrorIntercepterService implements HttpInterceptor{
         console.log('Http Request started');
         return next.handle(request)
         .pipe(
+            retryWhen(error=> this.retryRequest(error,10)),
             catchError((error:HttpErrorResponse)=>{
                 const errorMesssage=this.setError(error);
                 console.log(error);
@@ -23,6 +25,24 @@ export class HttpErrorIntercepterService implements HttpInterceptor{
         );
     }
 
+    retryRequest(error: Observable<any>, retryCount: number): Observable<any>
+    {
+        return error.pipe(
+            concatMap((checkErr:HttpErrorResponse, count:number)=>{
+                if (count<=retryCount) {
+                    switch (checkErr.status) {
+                        case ErrorCode.serverDown:
+                            return of(checkErr);
+
+                            case ErrorCode.unauthorized:
+                            return of(checkErr);
+                    }
+                }
+                return throwError(checkErr);
+            })
+        );
+    }
+    //retry the request in case of error 
     setError(error: HttpErrorResponse):string{
         let errorMesssage='Unknown error occured!';
         if (error.error instanceof ErrorEvent) {
